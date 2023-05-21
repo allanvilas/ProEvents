334ec115e2f4ff67174d932a25c6ce89609b0312
allanvilas <allanvilasdeveloper@gmail.com>
2023-05-21 20:04:26 -0300

    Add Sqlite db

---
+++ Back/src/ProEvents.API/Data/Migrations/20230521225722_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProEvents.API.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Local = table.Column<string>(type: "TEXT", nullable: true),
+                    DateEvent = table.Column<string>(type: "TEXT", nullable: true),
+                    QtdParticipants = table.Column<int>(type: "INTEGER", nullable: false),
+                    Theme = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    Lot = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
